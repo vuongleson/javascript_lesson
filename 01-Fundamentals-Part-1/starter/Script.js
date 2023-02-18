3352b41c50b53de9d10e358fd6b043638749d8e9
vuongleson <119582112+vuongleson@users.noreply.github.com>
2023-02-18 15:35:01 +0700

    Create Script.js

---
+++ 01-Fundamentals-Part-1/starter/Script.js
@@ -0,0 +1,66 @@
+/* let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+console.log("Son");
+console.log(23);
+// variable name convention
+let Firstname = "Vuong";
+console.log(Firstname); */
+/* let javascriptISFUN = true;
+console.log(typeof true);
+console.log(typeof javascriptISFUN);
+console.log(typeof 23);
+console.log(typeof "Jonas"); */
+/* const now = 2037;
+const ageSon = now - 1991;
+const ageSan = now - 2018;
+console.log(ageSon, ageSan);
+console.log(ageSon * 2, ageSon / 2, 2 ** 3);
+
+const firstName = "Vuong";
+const lastName = "Son";
+console.log(firstName + " " + lastName);
+
+let x = 10 + 5;
+x += 10;
+x *= 4;
+x++;
+console.log(x);
+console.log(ageSon > ageSan);
+console.log(ageSan > 18); */
+/* const now = 2037;
+const ageSon = now - 1991;
+const ageSan = now - 2018;
+console.log(now - 1991 > now - 2018);
+const averageAge = (ageSon + ageSan) / 2;
+console.log(ageSon, ageSan, averageAge); */
+/* const firstName = "Jonas";
+const job = "teacher";
+const birthYear = 1991;
+const year = 2037;
+const jonas =
+  "I'm " + firstName + ", a " + (year - birthYear) + " years old " + job + "!";
+console.log(jonas);
+const jonasNew = `I'm ${firstName}, a ${year - birthYear} years old ${job}!`;
+console.log(jonasNew);
+console.log(`String
+multiple
+lines`); */
+/* const age = 15; */
+/* const isOldEnough = age >= 18; */
+/* if (age >= 18) {
+  console.log("Sarah can start driving license 🚗");
+} else {
+  const yearsLeft = 18 - age;
+  console.log(`Sarah is too young. Wait another ${yearsLeft} years :)`);
+}
+const birthYear = 1991;
+let century;
+if (birthYear <= 2000) {
+  century = 20;
+} else {
+  century = 21;
+}
+console.log(century); */
+const inputYear = "1991";
+console.log(Number(inputYear), inputYear);
+console.log(Number(inputYear) + 18);
